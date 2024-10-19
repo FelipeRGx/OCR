25324e85b634e5cf36601cc203ca52512ba24799
Felipe_djrg <93945800+FelipeRGx@users.noreply.github.com>
2024-10-19 00:15:51 +0000

    Add files via upload

---
+++ image_utils.py
@@ -0,0 +1,115 @@
+import cv2
+import numpy as np
+import copy
+
+def read_image(image):
+    if type(image) == str:
+        img = cv2.imread(image)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    elif type(image) == bytes:
+        nparr = np.frombuffer(image, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    elif type(image) == np.ndarray:
+        if len(image.shape) == 2:  # grayscale
+            img = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+        elif len(image.shape) == 3 and image.shape[2] == 3:
+            img = image
+        elif len(image.shape) == 3 and image.shape[2] == 4:  # RGBAscale
+            img = image[:, :, :3]
+
+    return img
+
+
+def normalizeMeanVariance(
+    in_img, mean=(0.485, 0.456, 0.406), variance=(0.229, 0.224, 0.225)
+):
+    # should be RGB order
+    img = in_img.copy().astype(np.float32)
+
+    img -= np.array(
+        [mean[0] * 255.0, mean[1] * 255.0, mean[2] * 255.0], dtype=np.float32
+    )
+    img /= np.array(
+        [variance[0] * 255.0, variance[1] * 255.0, variance[2] * 255.0],
+        dtype=np.float32,
+    )
+    return img
+
+
+def denormalizeMeanVariance(
+    in_img, mean=(0.485, 0.456, 0.406), variance=(0.229, 0.224, 0.225)
+):
+    # should be RGB order
+    img = in_img.copy()
+    img *= variance
+    img += mean
+    img *= 255.0
+    img = np.clip(img, 0, 255).astype(np.uint8)
+    return img
+
+
+def resize_aspect_ratio(img, long_size, interpolation):
+    height, width, channel = img.shape
+
+    # set target image size
+    target_size = long_size
+
+    ratio = target_size / max(height, width)
+
+    target_h, target_w = int(height * ratio), int(width * ratio)
+    proc = cv2.resize(img, (target_w, target_h), interpolation=interpolation)
+
+    # make canvas and paste image
+    target_h32, target_w32 = target_h, target_w
+    if target_h % 32 != 0:
+        target_h32 = target_h + (32 - target_h % 32)
+    if target_w % 32 != 0:
+        target_w32 = target_w + (32 - target_w % 32)
+    resized = np.zeros((target_h32, target_w32, channel), dtype=np.float32)
+    resized[0:target_h, 0:target_w, :] = proc
+    target_h, target_w = target_h32, target_w32
+
+    size_heatmap = (int(target_w / 2), int(target_h / 2))
+
+    return resized, ratio, size_heatmap
+
+
+def cvt2HeatmapImg(img):
+    img = (np.clip(img, 0, 1) * 255).astype(np.uint8)
+    img = cv2.applyColorMap(img, cv2.COLORMAP_JET)
+    return img
+
+
+def draw_boxes(image, boxes, color=(0, 255, 0), thickness=2):
+    """draw boxes on image
+    Args:
+        image: path to the image to be processed or numpy array or PIL image
+        boxes (array): array of result file
+            Shape: [num_detections, 4] for BB output / [num_detections, 4]
+            for QUAD output
+    Return:
+        None
+    """
+    # read/convert image
+    image = read_image(image)
+
+    # deepcopy image so that original is not altered
+    image = copy.deepcopy(image)
+
+    # draw boxes
+    for i, box in enumerate(boxes):
+        box = np.array(box).astype(np.int32).reshape((-1))
+
+        box = box.reshape(-1, 2)
+        cv2.polylines(
+            image,
+            [box.reshape((-1, 1, 2))],
+            True,
+            color=color,
+            thickness=thickness,
+        )
+
+    return image
